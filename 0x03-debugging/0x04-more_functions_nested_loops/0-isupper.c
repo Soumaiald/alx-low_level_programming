e3de1bfa8bc6078d79d9f52b7df614aa233d8718
Soumaiald <soumaialoud@gmail.com>
2023-06-01 12:11:57 -0700

    isupper fct

---
+++ 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * isupper - returns 1 if true else returns 0
+ * @c: char
+ * Return: 1 or 0
+ */
+
+int _isupper(char c)
+{
+	if(isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
