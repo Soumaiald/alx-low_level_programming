0349c705629242248097e7f09b2a9844af6db09f
Soumaiald <soumaialoud@gmail.com>
2023-06-01 15:02:18 -0700

    adding mult fct

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplication function
+ * @a : integer 1
+ * @b : integer 2
+ * Return: the result of multipication of a @ b
+ */
+int mul(int a, int b)
+{	int c;
+	c=a*b;
+	return (c);
+}
